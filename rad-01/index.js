e4bfc8ef89047830f14269443bf6aaec359a6f48
sandula-sanchana <sandulasanchana@gmail.com>
2026-03-13 12:15:29 +0530

    first react app

--- rad-01/index.js
+++ rad-01/index.js
@@ -1,7 +1,9 @@
-let name="sandula";
-let age=25;
-let isStudent=true;
-
-console.log("Name: " + name);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "sandula";
+let age = 25;
+let isStudent = true;
+console.log("Username: " + username);
 console.log("Age: " + age);
 console.log("Is Student: " + isStudent);
+//# sourceMappingURL=index.js.map
