6ae585d7c43b1b6e30f3a7bace99905802324017
sandula-sanchana <sandulasanchana@gmail.com>
2026-03-20 14:25:51 +0530

    form

--- rad-03/src/App.tsx
+++ rad-03/src/App.tsx
@@ -1,121 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState,useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type User = {
+  name: string;
+  age: number;
+  email: string;
+};
+
+const App = () => {
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
+  const [email, setEmail] = useState<string>("");
+
+  const [userData, setUserData] = useState<User[]>([]);
+
+  useEffect(() => {
+  console.log(userData);
+}, [userData]);
+
+  function collectUsers(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const user: User = {
+      name,
+      age: Number(age),
+      email,
+    };
+
+    setUserData((prev) => [...prev, user]);
+
+    setName("");
+    setAge(0);
+    setEmail("");
+
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h3>Name : {name}</h3>
+      <h3>Age : {age}</h3>
+      <h3>Email : {email}</h3>
 
-      <div className="ticks"></div>
+      <form onSubmit={collectUsers} noValidate>
+        <input className="bg-pink-200"
+          type="text"
+          placeholder="username"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <input
+        className="bg-pink-600"
+          type="number"
+          placeholder="age"
+          value={age}
+          onChange={(e) => setAge(Number(e.target.value))}
+          required
+        />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
+        <input
+        className="bg-pink-800"
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-export default App
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
