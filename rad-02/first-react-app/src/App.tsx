1e1786f26e371eff7beace192a19469830dcac66
sandula-sanchana <sandulasanchana@gmail.com>
2026-03-20 10:06:23 +0530

    components

--- rad-02/first-react-app/src/App.tsx
+++ rad-02/first-react-app/src/App.tsx
@@ -1,22 +1,15 @@
 
 import './App.css'
+import { Title } from './components/Title'
 
 
 const App=()=>{
 
-  const name:string= "sandula"
-
   return (
-  <>
-   <h1 style={{
-     color:'red'
-   }} className='title'>
-    hello
-   </h1>
-   <h3>{name}</h3>
-   {name!==""?name:<h1>no name</h1>}
-  </>
-  )
+    <>
+      <Title />
+    </>
+  ) 
 
 }
 
---
+++ rad-02/first-react-app/src/components/Title.tsx
@@ -0,0 +1,16 @@
+
+export const Title=()=>{
+    const name:string= "sandula"
+
+  return (
+  <>
+   <h1 style={{
+     color:'red'
+   }} className='title'>
+    hello
+   </h1>
+   <h3>{name}</h3>
+   {name!==""?name:<h1>no name</h1>}
+  </>
+  )
+}
